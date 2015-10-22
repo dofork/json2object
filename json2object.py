626dcfeb9331974e7ce6ac8c66245b91f025bbcf
dofork <147796352@qq.com>
2015-10-22 14:00:11 +0800

    check json format

--- json2object.py
+++ json2object.py
@@ -20,10 +20,25 @@ class jsonObject:
 	implementation = "@implementation "
 	
 	object_prefix = "@property (nonatomic, strong) "
+	
+	primitive_prefix = "@property (nonatomic, assign) "
 	 
 	number_prefix =  " NSNumber <Optional> "
 	
+	number = " NSNumber "
+	
+	integer_prefix =  " NSInteger <Optional> "
+	
+	bool_prefix =  " BOOL <Optional> "
+	
+	float_prefix =  " float <Optional> "
+	
+	long_prefix =  " long <Optional> "
+	
 	string_prefix = " NSString <Optional> "
+	
+	string = " NSString "
+	
 	pointer = " * "
 	
 	class_seperator = "_"
@@ -34,9 +49,21 @@ class jsonObject:
 	
 	inherit_operator = " : "
 	
+	def printError(self, message):
+		print "Error : " + message
+		
+		
 	def __init__(self,file_name,root_class):
+	
 		self.file_name = file_name
-		self.parseJson(json.loads(self.readFile(self.file_name) ),root_class,False)
+		
+		try:
+			obj = json.loads(self.readFile(self.file_name) )
+			self.parseJson(obj,root_class,False)
+		except:
+			self.printError("invalid json string !")
+        	
+		
 		
 	def write2File(self):
 		
@@ -61,6 +88,8 @@ class jsonObject:
         	file_handler = open(file_name)
         	file_text = file_handler.read()
 		return file_text
+		
+	
 
 	def parseJson (self, decoded_json_string, class_name,isprotocol ):
 		if isprotocol == True :
@@ -69,8 +98,8 @@ class jsonObject:
 		str =  self.new_line + self.interface +  class_name + self.inherit_operator + self.base_class
 		for  k in decoded_json_string.keys():
 			v = decoded_json_string[k]
-			if isinstance(v,int) or isinstance(v,float) or isinstance(v,long) :
-				str += self.new_line + self.object_prefix + self.number_prefix + self.pointer + k + self.line_end
+			if isinstance(v,float) or isinstance(v,int) or isinstance(v,long) or isinstance(v,bool):
+				str += self.new_line + self.object_prefix + self.number_prefix + self.pointer+ k + self.line_end
 			elif isinstance(v,basestring) :
 				str += self.new_line + self.object_prefix +  self.string_prefix + self.pointer + k+ self.line_end
 			elif isinstance(v,dict):
@@ -78,18 +107,18 @@ class jsonObject:
 				str += self.new_line + self.object_prefix + class_name + self.pointer + k + self.line_end
 				self.parseJson(v,class_name,False)
 			elif isinstance(v,list) or isinstance(v,tuple) :
-				class_name = class_name + self.class_seperator + k
-				str += self.new_line + self.object_prefix + " NSMutableArray <" + class_name + ",Optional> * " + k + self.line_end
-				self.parseJson(v[0],class_name,True)
+				if len(v) == 0 :
+					self.printError( "key '" + k + "' can not be empty!");
+				value = v[0]
+				if isinstance(value,float) or isinstance(value,int) or isinstance(value,long) or isinstance(value,bool) or isinstance(value,basestring):
+					str += self.new_line + self.object_prefix + " NSMutableArray < Optional> * " + k + self.line_end
+				else:
+					class_name = class_name + self.class_seperator + k
+					str += self.new_line + self.object_prefix + " NSMutableArray <" + class_name + ",Optional> * " + k + self.line_end
+					self.parseJson(v[0],class_name,True)
 		str += self.new_line+self.end+self.new_line
 		self.class_define_array.append(str)
-			
-	
-	
-
-print "\n\nusage: \n python json2Object.py your_json_file your_base_class_name\n\n"
+		
 	
 object = jsonObject(sys.argv[1],sys.argv[2])
 object.write2File();
-
-print "\nsuccess!\n\n"
